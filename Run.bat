032770a3e8a61367e3557267873d63f51da4cd57
hime_alex <alexandergrahambell707@gmail.com>
2023-04-19 21:05:32 -0500

    fixes

--- Run.bat
+++ Run.bat
@@ -1,3 +1,14 @@
-cd %~dp0
-cmd /k python %~dp0\src\main.py
+:: Check for Python Installation
+python --version 2>NUL
+if errorlevel 1 goto errorNoPython
 
+:: Reaching here means Python is installed.
+cd %~dp0
+cmd /c python %~dp0\src\main.py
+
+:: Once done, exit the batch file -- skips executing the errorNoPython section
+goto:eof
+
+:errorNoPython
+echo.
+echo Error^: Python not installed
